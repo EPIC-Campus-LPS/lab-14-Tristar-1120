5c0d46cb2cee78dbe4889d04f54d1c45abbafd0c
Tristar-1120 <tristans139@lpsk12.org>
2025-11-07 09:46:38 -0700

    File Resend 11/7 9:47

---
+++ Heatmap.java
@@ -0,0 +1,80 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Heatmap {
+	private static double[][] data;
+	
+	public Heatmap(int rows, int cols, String filename) throws IOException {
+		Scanner s = new Scanner(new File(filename));
+		s.nextLine();
+		int r = 0;
+		while (s.hasNextLine()); {
+			String[] temp = s.nextLine().split(",");
+		    for (int i = 0; i<cols; i++) {
+			  data[r][i] = Integer.parseInt(temp[i]);
+		    }
+		    r++;
+		}
+		s.close();
+	}
+	
+	public void printHeatmap() {
+		for (double[] i:data) {
+			for (double j: i) {
+				System.out.print(j+"");
+			}
+		}
+	}
+	
+	public static double sumHeatmap() {
+		double sum = 0;
+		for (double[] i:data) {
+			for (double j:i) {
+				sum+=j;
+			}
+		}
+		return sum;
+	}
+	
+	public static double avgHeatmap() {
+		double sum = 0;
+		double count = 0;
+		for (double[] i:data) {
+			for (double j:i) {
+				sum += j;
+				count++;
+			}
+		}
+		return sum/count;
+	}
+	
+	public static double sumRow(int r) {
+		double sum = 0;
+		double[] row = data[r-1];
+		for (double j:row) {
+			sum+=j;
+		}
+		return sum;
+	}
+	
+	public static double sumColumn(int c) {
+		double sum = 0;
+		for (double[] i:data) {
+			sum += i[c-1];
+		}
+		return sum;
+	}
+	
+	public static int areCold() {
+		int count = 0;
+		for (double[] i:data) {
+			for (double j:i) {
+				if (j<10) {
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+}
